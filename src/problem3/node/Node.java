46771ebd9d422768b2f928b38807084c246cffc3
shikhar-sharma1703 <shikhar.sharma.53@gmail.com>
2020-03-31 16:46:56 +0530

    Assignment

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,33 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
 package problem3.node;
 
+import problem3.Student;
+
 public class Node {
+    private Student data;
+    private Node next;
+
+    public Node(Student d) {
+        data = d;
+        next = null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Student getData() {
+        return data;
+    }
+
+    public void setData(Student data) {
+        this.data = data;
+    }
+
+    public int compareTo(Student data) {
+        return data.getRollNo() - this.data.getRollNo();
+    }
 }
--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,10 +1,29 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.node;
-// to define node properties
+
+import problem5.student.Student;
+
 public class Node {
+    private Student data;
+    private Node next;
+
+    public Node(Student data) {
+        this.data = data;
+        next = null;
+    }
+
+    public Student getData() {
+        return data;
+    }
+
+    public void setData(Student data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
